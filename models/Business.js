3242a1533d294a53e78464900eace12ae467a2ef
Colton Self <clself05@gmail.com>
2026-03-12 15:35:48 -0400

    Create Business Schema

---
+++ models/Business.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const connection = mongoose.createConnection('mongodb://localhost:27017/Marketplace');
+
+const businessSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    ownerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+    },
+    image: {
+        type: String
+    },
+    address: {
+        type: String
+    },
+    phone: {
+        type: String
+    },
+    reviewCount: {
+        type: Number,
+        default: 0
+    },
+    averageReviewScore: {
+        type: Number,
+        default: 0
+    }
+});
+
+const Business = connection.model('Business', businessSchema);
+
+module.exports = Business;
