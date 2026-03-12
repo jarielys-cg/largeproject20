822fbe330152747c2b738d7f11e4b344f4d9a277
Colton Self <clself05@gmail.com>
2026-03-12 15:36:08 -0400

    Create Review Schema

---
+++ models/Review.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const connection = mongoose.createConnection('mongodb://localhost:27017/Marketplace');
+
+const reviewSchema = new mongoose.Schema({
+    businessId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Business',
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    review: {
+        type: String
+    }
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
