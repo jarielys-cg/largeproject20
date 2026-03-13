8cc4c6b9c64dd409cce5d2b1382345c65121de97
Colton Self <clself05@gmail.com>
2026-03-13 14:24:12 -0400

    Switch to using external database

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
+
 const User = require("./models/User");
 const Business = require("./models/Business");
 const Review = require("./models/Review");
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost:27017/Marketplace')
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+
+mongoose.connect(uri)
     .then(() => {
-        console.log('MongoDB connected');
+        console.log("MongoDB connected");
     })
-    .catch((err) => {
-        console.log('Error connecting to MongoDB:', err);
-    });
+    .catch(err => console.error("MongoDB connection error:", err));
+
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
