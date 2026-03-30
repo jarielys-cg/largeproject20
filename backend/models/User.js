f33fffb51355fe26d21ceed81a46f6c5fe58d62b
TheMconquer <tadmukonka@yahoo.com>
2026-03-29 22:40:58 -0400

    Updated User.js file from commonJS to ESM

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -24,6 +24,6 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-const User = connection.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports = User;
+export default User;
