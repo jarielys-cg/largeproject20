d0ef77770928d765e297c3034057133e022f5092
Jarielys Cruz Gomez <jarielys.cg@gmail.com>
2026-04-04 13:31:29 -0400

    Added descending order and offset-based pagination

--- backend/api/search.ts
+++ backend/api/search.ts
@@ -6,20 +6,43 @@ export const searchBusiness = async(req: Request, res: Response) => {
     try {
 
         // GET request, handles empty string
-        const search = (req.query.search as string) || "";
+        const search = ((req.query.search as string) || "").trim();
 
         // If search is empty, return all businesses
-        const results = search ? { $or : [ {name : { $regex: search, $options: "i" }}, { category : { $regex: search, $options: "i" } } ] } : {};
+        const results = search 
+        ? { 
+            $or : [
+                { name : { $regex: search, $options: "i" } }, 
+                { category : { $regex: search, $options: "i" } }
+            ] 
+        } 
+        : {};
 
-        const businesses = await Business.find(results);
+        // Pagination, displays 20 businesses per page
+        const page = Math.max(1, parseInt(req.query.page as string) || 1);
+        const pageSize = 20;
+        const skip = (page - 1) * pageSize;
 
-        res.json(businesses);
+        // Stores all business in descending order 
+        const[businesses, total] = await Promise.all ([
+            Business
+                .find(results)
+                .sort({ averageReviewScore: -1 })
+                .skip(skip)
+                .limit(pageSize),
+
+            Business.countDocuments(results)
+        ]);
+
+        res.json({
+            data: businesses,
+            page,
+            totalPages: Math.ceil(total / pageSize),
+            total
+        });
 
     }catch (err) {
         console.error("Search error: ", err);
         res.status(500).json({ error: "Server error" });
     }
-}; 
-
-// Add pagination
-// Display highest to lowest rating
+};
