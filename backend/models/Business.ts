ef54a495912a695ebc28578edbf26e496d579b75
Colton Self <clself05@gmail.com>
2026-04-06 10:37:15 -0400

    Change database files to typescript

---
+++ backend/models/Business.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const businessSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    ownerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+    },
+    image: {
+        type: String
+    },
+    address: {
+        type: String
+    },
+    phone: {
+        type: String
+    },
+    websiteLink: {
+        type: String
+    },
+    reviewCount: {
+        type: Number,
+        default: 0
+    },
+    averageReviewScore: {
+        type: Number,
+        default: 0
+    }
+});
+
+const Business = mongoose.model("Business", businessSchema);
+
+export default Business;
