9228ffb45fe594a53c78c1c06bd8d3471d50c62b
Wesley Wild <wdw0510@gmail.com>
2026-04-07 20:33:53 -0400

    Add business endpoint

--- backend/api/addB.ts
+++ backend/api/addB.ts
@@ -3,5 +3,32 @@ import Business from "../models/Business.js";
 
 export const addB = async (req: Request, res: Response) => 
 {
+    const { name, ownerId, category, description, image, address, phone, websiteLink,  } = req.body;
 
+    const newBusiness = new Business({
+        name,
+        ownerId,
+        category,
+        description,
+        image,
+        address,
+        phone,
+        websiteLink
+    });
+
+    const existingBusiness = await Business.findOne({ ownerId: newBusiness.ownerId, name: newBusiness.name });
+    if(existingBusiness)
+    {
+        return res.status(400).json({ error: "Business name already exists under this account" });
+    }
+
+    try 
+    {
+        const savedB = await newBusiness.save(); // saves to MongoDB
+        res.status(201).json(savedB);       // respond with the saved user
+    } 
+    catch (err) 
+    {
+        return res.status(400).json({ error: "Error adding to database" });
+    }
 }
