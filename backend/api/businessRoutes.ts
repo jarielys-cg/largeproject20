a81e6a7c870e03858704ff586102e5b59ee8caf7
Jarielys Cruz Gomez <jarielys.cg@gmail.com>
2026-04-10 14:51:51 -0400

    Made changes to search API to match main branch

--- backend/api/businessRoutes.ts
+++ backend/api/businessRoutes.ts
@@ -3,9 +3,11 @@ const router = express.Router();
 import { addB } from "./addB.js";
 import { editB } from "./editB.js";
 import { removeB } from "./removeB.js";
+import { searchBusiness } from "./search.js";
 
 router.post("/addB", addB);
 router.patch("/editB", editB);
 router.delete("/removeB", removeB);
+router.post("/search", searchBusiness);
 
 export default router;
