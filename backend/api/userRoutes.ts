087851c98ed9657def5c668e5c4af69af10a226c
Wesley Wild <wdw0510@gmail.com>
2026-04-10 18:37:18 -0400

    password reset endpoint

--- backend/api/userRoutes.ts
+++ backend/api/userRoutes.ts
@@ -2,8 +2,10 @@ import express from "express";
 const router = express.Router();
 import { signUp } from "./signUp.js";
 import { login } from "./login.js";
+import { passwordReset } from "./passwordReset.js";
 
 router.post("/signUp", signUp);
 router.post("/login", login);
+router.patch("/passwordReset", passwordReset);
 
 export default router;
